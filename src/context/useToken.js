1ac088da1860068a02db23c339f825be3457afda
afaf98 <afaf.ibrahimi@gmail.com>
2021-03-05 12:31:31 +0100

    Signup form redirect to profile page

--- src/context/useToken.js
+++ src/context/useToken.js
@@ -1,24 +1,27 @@
 import { createContext, useState, useContext } from "react";
 import getUser from "../services/login";
+import newUser from "../services/signup";
 
 const TokenContext = createContext({});
 
 export function TokenProvider(props) {
   const [token, setToken] = useState(localStorage.getItem("token")); // move it to a dedicated component
 
-  const login = async (data) => {
-    const response = await getUser(data);
+  const auth = async (data, service) => {
+    const serviceFunction = service === "login" ? getUser : newUser;
+
+    const response = await serviceFunction(data);
     if (response.error) {
       return { status: "Error", message: response.error };
     } else {
-      localStorage.setItem("token", response.token);
       setToken(response.token);
+      localStorage.setItem("token", response.token);
       return { status: "Success", message: response.message };
     }
   };
 
   return (
-    <TokenContext.Provider value={{ token: token, login: login }}>
+    <TokenContext.Provider value={{ token: token, auth: auth }}>
       {props.children}
     </TokenContext.Provider>
   );
