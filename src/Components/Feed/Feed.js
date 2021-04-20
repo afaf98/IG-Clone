a10158363a18df509b6d8c4c8caad1a0ec8c4d3d
afaf98 <afaf.ibrahimi@gmail.com>
2021-04-20 11:58:06 +0200

    Responsive style feed + removing warnings

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import useToken from "../../context/useToken";
 import getFeed from "../../services/getFeed";
 import "./Feed.css";
@@ -6,28 +6,27 @@ import "./Feed.css";
 export default function Feed(props) {
   const { token } = useToken();
   const [posts, setPosts] = useState(null);
-  const [users, setUsers] = useState(null);
-  async function feed() {
+
+  const feed = useCallback(async () => {
     const response = await getFeed(token);
-    // console.log("rs", response);
     setPosts(response.images);
-    setUsers(response.users);
-  }
-  console.log("users", users && users[0].id);
+  }, [token]);
 
   useEffect(() => {
     feed();
-  }, []);
+  }, [feed]);
+
   return (
     <div className="feed-page">
+      <h2 className="title-feed">New pictures posted!</h2>
       {posts &&
         posts.map((post, index) => {
           return (
-            <div className="post-card">
+            <div key={index} className="post-card">
               <div className="user-name">
                 {post.userFirstName} {post.userLastName}
               </div>
-              <img src={post.url} className="post-image" />
+              <img src={post.url} alt="post" className="post-image" />
             </div>
           );
         })}
